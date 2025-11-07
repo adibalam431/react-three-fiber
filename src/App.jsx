3aa422019b15789a301fc433f247624ed290edcb
adibalam431 <adibalam431@gmail.com>
2025-11-07 22:02:40 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import "./style.css";
 import { OrbitControls, useTexture } from "@react-three/drei";
 import * as three from "three";
 import Scene from "./scene";
+import {
+  Bloom,
+  EffectComposer,
+  ToneMapping,
+} from "@react-three/postprocessing";
+import { BlurPass, Resizer, KernelSize, Resolution } from "postprocessing";
 
 function App() {
-  
   return (
-    <Canvas>
+    <Canvas flat camera={{fov:35}}>
       <OrbitControls />
       <ambientLight />
       <Scene />
+      <EffectComposer>
+        <Bloom
+
+          mipmapBlur // Enables or disables mipmap blur.
+          intensity={7} // The bloom intensity.
+          luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+        
+      </EffectComposer>
     </Canvas>
   );
 }
--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Canvas } from "@react-three/fiber";
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./style.css";
 import { OrbitControls, useTexture } from "@react-three/drei";
 import * as three from "three";
 
+
 const Scene = () => {
-    const [img1,img2,img3] = useTexture(["../public/vite.svg","../public/vite.svg","../public/vite.svg"])
+    const [img] = useTexture(["../public/img2 (2).png"]);
+    const scene = useRef();
+    useFrame((state,delta)=>{
+scene.current.rotation.y += delta;
+    })
   return (
-    <mesh>
-        <cylinderGeometry args={[1,1,1,25,25,true]} />
-        <meshBasicMaterial map={img1 img2} side={three.DoubleSide} />
+    <mesh ref={scene} rotation={[0,1.2,0.3]}>
+        <cylinderGeometry args={[1.5,1.5,6,60,60,true]} />
+        <meshBasicMaterial transparent map={img} side={three.DoubleSide} />
       </mesh>
   )
 }
